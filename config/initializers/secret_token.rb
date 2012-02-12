25bb17a00998262c2e6c3c5f78eb1aeadae60f0d
vvdpzz <vvdpzz@gmail.com>
2012-02-12 14:24:36 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pxlips::Application.config.secret_token = '7b43edd4bc99adc6a90634a0b932e1a6d56aeeb8a4cd3cf5f8ac166d4bf748251ad7e9ffe7c2cc10f4567c5f4d0412b98e9c34528fde9e490cd08b174ffd0e7b'
